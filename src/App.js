aaf6bd0ae9fb4c019b9f65c15b40fe177bb957b0
Naftaly <80479583+NaftalyMurimi@users.noreply.github.com>
2021-09-28 17:26:30 +0300

    delete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 
 import Advice from './Advice';
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           The Farmer says:
         </p>
